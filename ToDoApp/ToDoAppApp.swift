ab99c36416dc939fe22edfd6068c0bec356c2efb
Ilya Senchukov <senchukov02@gmail.com>
2020-10-27 17:52:50 +0200

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Ilya Senchukov on 27.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
